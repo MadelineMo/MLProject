6537d4c9e387b2c337ca82e6b1bc26b1b5141517
MadelineMo <71033349+MadelineMo@users.noreply.github.com>
2022-11-10 19:24:29 -0600

    New data - Initialized

--- main.py
+++ main.py
@@ -8,18 +8,10 @@ from initialize import initalize
 
 # Initialized data
 # returns standardized data
-X, y_4_Math, y_8_Math, y_4_Reading, y_8_Reading = initalize()
+X, y = initalize()
 
 # split data into train and test sets (per y set)
-X_4_Math_train, X_4_Math_test, y_4_Math_train, y_4_Math_test = train_test_split(
-    X, y_4_Math, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
 
-X_8_Math_train, X_8_Math_test, y_8_Math_train, y_8_Math_test = train_test_split(
-    X, y_8_Math, test_size=0.3, random_state=0)
-
-X_4_Reading_train, X_4_Reading_test, y_4_Reading_train, y_4_Reading_test = train_test_split(
-    X, y_4_Reading, test_size=0.3, random_state=0)
-
-X_8_Reading_train, X_8_Reading_test, y_8_Reading_train, y_8_Reading_test = train_test_split(
-    X, y_8_Reading, test_size=0.3, random_state=0)
 
